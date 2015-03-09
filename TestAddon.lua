e8f750e674e3e43319d97910309b0edd205b5044
Mike Rheault <mike.rheault@karmaloop.com>
2015-03-09 11:18:57 -0400

    Update TestAddon.lua

--- TestAddon.lua
+++ TestAddon.lua
@@ -1,12 +1,7 @@
--- First, we create a namespace for our addon by declaring a top-level table that will hold everything else.
 TestAddon = {}
  
--- This isn't strictly necessary, but we'll use this string later when registering events.
--- Better to define it in a single place rather than retyping the same string.
 TestAddon.name = "TestAddon"
  
--- Next we create a function that will initialize our addon
-
 function TestAddon:RestorePosition()
   local left = self.savedVariables.left
   local top = self.savedVariables.top
@@ -27,9 +22,7 @@ function TestAddon:Initialize()
 end
 
 function TestAddon.OnPlayerCombatState(event, inCombat)
-  -- The ~= operator is "not equal to" in Lua.
   if inCombat ~= TestAddon.inCombat then
-    -- The player's state has changed. Update the stored state...
     TestAddon.inCombat = inCombat
  
     TestAddonIndicator:SetHidden(not inCombat)
@@ -42,14 +35,10 @@ function TestAddon.OnIndicatorMoveStop()
   TestAddon.savedVariables.top = TestAddonIndicator:GetTop()
 end
  
--- Then we create an event handler function which will be called when the "addon loaded" event
--- occurs. We'll use this to initialize our addon after all of its resources are fully loaded.
 function TestAddon.OnAddOnLoaded(event, addonName)
-  -- The event fires each time *any* addon loads - but we only care about when our own addon loads.
   if addonName == TestAddon.name then
     TestAddon:Initialize()
   end
 end
  
--- Finally, we'll register our event handler function to be called when the proper event occurs.
 EVENT_MANAGER:RegisterForEvent(TestAddon.name, EVENT_ADD_ON_LOADED, TestAddon.OnAddOnLoaded)
